774b99ff07d212d760073efb04a389de4fcf6684
ashcl0ud712 <itsasoby@icloud.com>
2022-09-28 22:16:54 +0700

    Initial Commit

---
+++ PISIDBMQ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PISIDBMQ
+//
+//  Created by Asoby ‎‎ on 28.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
